d2925e640653103eeaa6f3f1fdb0b492af842dfd
eng-alimallouhe <engalimallouhe@gmail.com>
2026-07-30 16:19:20 -0700

    fix(spacing-bug): fix overflow bug in the UI

--- SNS.Web/src/app/app.config.ts
+++ SNS.Web/src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { provideTranslateService } from '@ngx-translate/core';
-import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
 import { authInterceptor } from './identity/shared/interceptors/auth.interceptor';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,10 +15,28 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideTranslateService({
       fallbackLang: 'en',
-      loader: provideTranslateHttpLoader({
-        prefix: '/assets/i18n/',
-        suffix: '.json'
-      })
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
     })
   ]
 };
+
+
+export class CustomTranslateHttpLoader implements TranslateLoader {
+  constructor(
+    private http: HttpClient,
+    private prefix: string = './assets/i18n/',
+    private suffix: string = '.json'
+  ) { }
+
+  public getTranslation(lang: string): Observable<any> {
+    return this.http.get(`${this.prefix}${lang}${this.suffix}?v=${environment.appVersion}`);
+  }
+}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new CustomTranslateHttpLoader(http, './assets/i18n/', '.json');
+}
--- SNS.Web/src/environments/environment.development.ts
+++ SNS.Web/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     apiUrl: "http://169.58.76.150:5000/api/v1/",
-    production: false
+    production: false,
+    appVersion: '1.0.1'
 };
