e265effa8674355f4b0e8f5c9d224dc8c0e6cce7
eng-alimallouhe <engalimallouhe@gmail.com>
2026-08-07 23:32:56 -0700

    feature(domain-name): added domain name and linked in docker compose with Caddy service]

--- SNS.Web/src/environments/environment.ts
+++ SNS.Web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: "http://169.58.76.150:5000/api/v1/",
+    apiUrl: "https://syrian-devs.duckdns.org/api/v1/",
     production: true,
     appVersion: '1.0.1.1'
 };
